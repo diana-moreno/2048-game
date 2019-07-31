2ea6eb43e2a7a36f3c945699fa224d41194500e4
Diana <d7@hotmail.es>
2019-07-31 14:04:21 +0200

    class BoardGame complete

--- src/index-oop.js
+++ src/index-oop.js
@@ -1,17 +1,40 @@
 class BoardGame {
   constructor() {
-    this.board = Array(4).fill(null).map(()=>Array(4).fill(null));
+    this.board = Array(4).fill().map(()=>Array(4).fill(null));
+    this.setRandomTile();
+    this.setRandomTile();
+    this.consoleBoard();
   }
-  getEmptyPosition() {
-
+  getAllEmptyPositions() {
+    let emptyPositions = [];
+    this.board.forEach(function(row, indexRow) {
+      row.forEach(function(elem, indexElem) {
+        if(!elem) {
+          emptyPositions.push({x: indexRow, y: indexElem});
+        }
+      })
+    })
+    return emptyPositions;
+  }
+  getEmptyRandomPosition() {
+    let emptyPositions = this.getAllEmptyPositions();
+    if(emptyPositions.length > 0) {
+      let randomPosition = Math.floor(Math.random() * emptyPositions.length);
+      console.log(emptyPositions[randomPosition]);
+      return emptyPositions[randomPosition];
+    }
   }
   setRandomTile() {
-
+    const emptyRandomPosition = this.getEmptyRandomPosition();
+    let randomNumber = (Math.random() < 0.8) ? 2 : 4; // This value has an 80% chance to be 2, and just a 20% chance to be 4.
+    this.board[emptyRandomPosition.x][emptyRandomPosition.y] = randomNumber;
   }
   consoleBoard() {
-
+    this.board.forEach(row => console.log(row));
   }
 }
+
+let boardGame = new BoardGame()
 
 class Game2048 {
   constructor() {
